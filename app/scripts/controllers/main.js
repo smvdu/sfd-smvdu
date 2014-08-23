5d0e538630ecdc45df1ed88dc488c0860846c8df
rishy <rishy.s13@gmail.com>
2014-08-23 06:05:21 +0530

    modified landing page and footer

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -15,28 +15,4 @@ angular.module('sfdSmvduApp')
       'AngularJS',
       'Karma'
     ];
-
-/*    $scope.scrollTo = function(id) {
-    	var element =  document.getElementById(id);
-    	var pos = element.offsetTop;
-    	var windowPos = $(document).scrollTop();
-      var timeInterval;
-
-      var intervalId = setInterval(function(){
-        if(windowPos < pos) {
-          window.scrollTo(0, windowPos += 10);
-          timeInterval = pos - windowPos;
-          if(windowPos >= pos) {
-            clearInterval(intervalId);
-          }
-        } else {
-          window.scrollTo(0, windowPos -= 10);
-          timeInterval = windowPos - pos;
-          if(windowPos <= pos) {
-            clearInterval(intervalId);
-          }
-        }
-      }, timeInterval);
-    };
-*/
   });
