861815c2957d6d163f049511689b24f5bfdd33ee
rishy <rishy.s13@gmail.com>
2014-08-24 18:10:19 +0530

    added animations on scroll

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,12 +10,7 @@
 
 angular.module('sfdSmvduApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-
+   
     $scope.summary = [];
 
     $scope.showSummary = function(index) {
--- app/scripts/directives/myscrolldirective.js
+++ app/scripts/directives/myscrolldirective.js
@@ -17,15 +17,26 @@ angular.module('myDirectives')
 	        home = document.getElementById('home');
 	        header = document.getElementById('main-header');
 	        hHeight = home.offsetHeight;
-	        window.onscroll = function() {
-	          var scrollTop;
+	        var sections = document.getElementsByTagName('section');
+	        var scrollTop;
+
+	        window.onscroll = function() {	
 	          scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
 	          if (scrollTop > hHeight - header.offsetHeight/2) {
 	            header.setAttribute('class', 'shrink');
 	          } else {
 	            header.setAttribute('class', '');
 	          }
+	          for(var i = 0; i < sections.length; i++) {
+		        	if(sections[i].offsetTop === scrollTop) {
+		        		for(var j = 0; j < sections.length; j++) {
+		        			sections[j].children[0].className = '';
+		        		}
+		        		sections[i].children[0].className = 'fadeIn';
+		        	}
+	        	}
 	        };
+	        
       	}
   	};
   });
