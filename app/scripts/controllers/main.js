6c74c74ae77a589cd0e88b9add398a3ad796e9ab
rishy <rishy.s13@gmail.com>
2014-08-24 15:57:03 +0530

    added event page and fixed responsiveness

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -15,4 +15,75 @@ angular.module('sfdSmvduApp')
       'AngularJS',
       'Karma'
     ];
+
+    $scope.summary = [];
+
+    $scope.showSummary = function(index) {
+      $scope.summary[index] = true;
+    };
+
+    $scope.hideSummary = function(index) {
+      $scope.summary[index] = false;
+    };
+
+    var date20 = '20th September';
+    var place1 = 'Sanskriti Kaksh', place2 = 'Lecture Hall-D', place3 = 'Internet Lab';
+    var listApart = {
+        'time': '',
+        'date': '',
+        'place': '',
+        'title': '',
+        'summary': ''
+      };
+
+    $scope.events = [
+      {
+        'time': '10:30 - 11:30',
+        'date': date20,
+        'place': place1,
+        'title': 'Inaugural Ceremony',
+        'summary': 'Inaugural Ceremony for SFD 2014 at SMVDU.' + 
+         ' Convener and Chief Guest will address the participating students.'
+      },
+      listApart,
+      {
+        'time': '11:45 - 13:15',
+        'date': date20,
+        'place': place2,
+        'title': 'Linux Workshop',
+        'summary': 'Linux Workshop will cover the basics of linux installation and usage.'
+      },
+      listApart,
+      {
+        'time': '13:15 - 14:00',
+        'date': date20,
+        'place': '',
+        'title': 'Lunch',
+        'summary': ''
+      },
+      listApart,
+      {
+        'time': '14:00 - 15:30',
+        'date': date20,
+        'place': place3,
+        'title': 'Python Workshop',
+        'summary': 'Basics of Python. Why Python is all the rage right now.'
+      },
+      listApart, 
+      {
+        'time': '15:45 - 17:00',
+        'date': date20,
+        'place': place2,
+        'title': 'Wikipedia Workshop',
+        'summary': 'How to transform from just an user to a contributor of one of the ' +
+        'most informative resource of internet - Wikipedia.'
+      }
+    ];
+
+    $scope.checkListApart = function(index) {
+      if( index%2 !== 0) {
+        return 'list-apart';
+      }
+    };
+
   });
